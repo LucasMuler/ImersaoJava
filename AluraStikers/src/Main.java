a81d32653194698b37480fdeaed3a3c01e0c1bbe
Lucas Muller <miillerclei@hotmail.com>
2023-06-13 22:45:03 -0300

    Alterando o texto

--- AluraStikers/src/Main.java
+++ AluraStikers/src/Main.java
@@ -3,6 +3,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.Key;
 import java.util.List;
 import java.util.Map;
 
@@ -35,7 +36,24 @@ public class Main {
         JsonParser parser = new JsonParser();
         List<Map<String,String>> listaDeFilmes = parser.parse(body);
 
-        //--------------------------------------------------------------------------------------------------
+
+        /*
+            Trabalhando com as Strings
+            --------------------------------------------------------------------------------------------------
+         */
+
+        for (Map<String,String> filme: listaDeFilmes) {
+            System.out.println("\u001b[371m \u001b[45mClassificação:" + filme.get("imDbRating") + "\u001b[m");
+
+            int classificacao = (int) Double.parseDouble(filme.get("imDbRating"));
+            for(int i = 0; i < classificacao; i++){
+                System.out.print("⭐");
+            }
+            System.out.println();
+            System.out.println("Titulo : " + filme.get("title"));
+            System.out.println("Poster : " + filme.get("image"));
+            System.out.println();
+        }
 
     }
 }
